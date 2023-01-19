9f5d6d458ca991f697f01424a17dee583aa56fa0
ShreyParikh07 <sparikh@jk-rx200-1.gcm.genzentrum.lmu.de>
2023-01-19 16:08:59 +0100

    Final Changes

--- Classifier/ScraMF_PBMC.py
+++ Classifier/ScraMF_PBMC.py
@@ -15,157 +15,62 @@ from matplotlib import rcParams
 import seaborn as sb
 import matplotlib.pyplot as plt
 
+
 package_dir = os.path.dirname(__file__)
 
-def train_model_classifer(adata_training, epochs=20, max_depth=10, eta=0.15, predict=False):
-    """ Train models for classifying cells into males and females through sex_classifier_universal function"""
-    start_time = time.time()
-    print('Initializing')
-    warnings.simplefilter("ignore", UserWarning)
-    warnings.simplefilter("ignore", RuntimeWarning)
-    warnings.simplefilter("ignore", FutureWarning)
-    
-    print('Preparing Training Anndata')
-    #Add Sex_Class to training adata if it does not exist
-    adata_training_copy = adata_training.copy()
-    adata_training_copy.obs.columns = adata_training_copy.obs.columns.str.capitalize()
-    sex_classes = {'F':0, 'M':1}
-    if 'Sex' in adata_training_copy.obs_keys():
-        adata_training_copy.obs['Sex_Class'] = adata_training_copy.obs.Sex.map(sex_classes).astype('category')
-    else:
-        print('Please provide training Anndata with SEX column')
-    
-    print('Training Models')
-    
-    #Load Parameters:
-    train = xgb.DMatrix(adata_training_copy.X, label=adata_training_copy.obs.Sex_Class)
-    param_softmax = {'max_depth':max_depth, 'eta':eta, 'objective':'multi:softmax','num_class':2}
-    param_softprob = {'max_depth':max_depth, 'eta':eta, 'objective':'multi:softprob','num_class':2}
-    #Train Models
-    model_softmax = xgb.train(param_softmax, train, epochs)
-    model_softprob = xgb.train(param_softprob, train, epochs)
-    
-    #Make gene list
-    #Select ensemble IDs if they are present instead of gene names
-    adata_training_copy.var = adata_training_copy.var.astype(str)
-    global genes_training
-    for i,j in zip(adata_training_copy.var.iloc[0,:], range(0,(len(adata_training_copy.var.iloc[0,:])))):
-        if 'ENS' in i:
-            genes_training = adata_training_copy.var.iloc[:,j].tolist()
-            break
-        else:
-            genes_training = adata_training_copy.var.index.tolist()
-    print('Training Complete')
-    
-    if predict==True:
-        #Make the test matrix on training adata
-        test = xgb.DMatrix(adata_training_copy.X, label=adata_training_copy.obs.Sex_Class)
-        #Add the predictions to the copy of the training adata
-        predictions_softmax = model_softmax.predict(test)
-        predictions_softprob = model_softprob.predict(test)
-        #Add columns to obs of training adata
-        adata_training_copy.obs['Predictions'] = predictions_softmax
-        adata_training_copy.obs['Predictions_Probability_Female'] = predictions_softprob.T[0]
-        adata_training_copy.obs['Predictions_Probability_Male'] = predictions_softprob.T[1]
-        adata_training_copy.obs['Class_Prediction'] = np.where((adata_training_copy.obs['Predictions'] == 0.0), 
-                                                  adata_training_copy.obs.Predictions_Probability_Female, 
-                                                  adata_training_copy.obs.Predictions_Probability_Male)
-        adata_training_copy.obs.Sex_Class = adata_training_copy.obs.Sex_Class.astype(float)
-        adata_training_copy.obs.Predictions = adata_training_copy.obs.Predictions.astype(float)
-        #Split the copy of training adata into males and females
-        females = adata_training_copy[adata_training_copy.obs.Sex == 'F']
-        males = adata_training_copy[adata_training_copy.obs.Sex == 'M']
-        values = [0.75, 0.80, 0.85, 0.90, 0.95]
-        dict_names = ['a', 'b', 'c', 'd', 'e']
-        adata_list = [females, males]
-        df_plot = ['df_females_plot', 'df_males_plot']
-        for adata, df_name in zip(adata_list, df_plot):
-            for cutoff,name in zip(values, dict_names):
-                conditions = [((adata.obs.Sex_Class == adata.obs.Predictions) & (adata.obs.Class_Prediction > cutoff)),
-                  ((adata.obs.Sex_Class != adata.obs.Predictions) & (adata.obs.Class_Prediction > cutoff)), 
-                  ((adata.obs.Sex_Class == adata.obs.Predictions) & (adata.obs.Class_Prediction < cutoff)),
-                  ((adata.obs.Sex_Class != adata.obs.Predictions) & (adata.obs.Class_Prediction < cutoff))]
-                choices = [ 'True Positive', 'False Positive', 'False Negative' , 'True Negative']
-                adata.obs["Condition"] = np.select(conditions, choices, default=np.nan)
-                globals()['cutoff' + '_' + name ] = adata.obs.groupby('Condition').size().to_dict()
-            cutoff_values = ['Cutoff: 0.75','Cutoff: 0.80', 'Cutoff: 0.85', 'Cutoff: 0.90', 'Cutoff: 0.95']
-            globals()[df_name] = pd.DataFrame([cutoff_a, cutoff_b, cutoff_c,cutoff_d, cutoff_e], index=cutoff_values)
-#             globals()[df_name] = globals()[df_name][['True Positive', 'True Negative','False Positive', 'False Negative']]
-#             globals()[df_name]['Total'] = globals()[df_name].sum(axis=1)
-        
-        #Plot with Group
-        fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, figsize=(10,8))
-        fig = plt.figure()
-        fig.subplots_adjust(hspace=0.5, wspace=0.6)
-        sb.set_style(style='whitegrid')
-        sb.lineplot(data=(df_females_plot.iloc[:,:4]['True Positive'], df_females_plot.iloc[:,:4]['True Negative'], df_females_plot.iloc[:,:4]['False Positive'], 
-                          df_females_plot.iloc[:,:4]['False Negative']), 
-                   ax=ax1, dashes=False, markers=True, linewidth = 3).set(title="Ground Truth: Females")
-        sb.lineplot(data=(df_males_plot.iloc[:,:4]['True Positive'], df_males_plot.iloc[:,:4]['True Negative'], df_males_plot.iloc[:,:4]['False Positive'], 
-                          df_males_plot.iloc[:,:4]['False Negative']), 
-                   ax=ax2, dashes=False, markers=True, linewidth = 3).set(title="Ground Truth: Females")
-        plt.show()
-        
-        #Accuracy Score
-        acc_score_males = accuracy_score(males.obs.Sex_Class, males.obs.Predictions)
-        print('The accuracy_score for males for universally trained model is {}'.format(acc_score_males))
-        acc_score_females = accuracy_score(females.obs.Sex_Class, females.obs.Predictions)
-        print('The accuracy_score for females for universally trained model is {}'.format(acc_score_females))
-        #Add the columns to the original adata
-        sex_classes_2 = {0: 'F', 1: 'M'}
-        adata_training.obs['Predictions'] = predictions_softmax
-        adata_training.obs.Predictions = adata_training.obs.Predictions.map(sex_classes_2)
-        adata_training.obs['Predictions_Probability_Female'] = predictions_softprob.T[0]
-        adata_training.obs['Predictions_Probability_Male'] = predictions_softprob.T[1]
-    else:
-        pass
-    print("--- %s mins ---" % int((time.time() - start_time)/60))
-    return model_softmax, model_softprob
-    
-    
-def sex_classifier_universal(adata_test, model_softmax, model_softprob, class_prob_cutoff=0.85):
+def scramf_pbmc(adata, class_prob_cutoff=0.85):
     """ Classifies cells into males and females based on the gene expression"""
     start_time = time.time()
     print('Initializing')
     warnings.simplefilter("ignore", UserWarning)
     warnings.simplefilter("ignore", RuntimeWarning)
     warnings.simplefilter("ignore", FutureWarning)
+    #Load the models
+    model_softmax = xgb.Booster()
+    model_softmax.load_model(os.path.join(package_dir, 'model_softmax_final.json'))
+    model_softprob = xgb.Booster()
+    model_softprob.load_model(os.path.join(package_dir, 'model_softprob_final.json'))
+    #Load the gene list
+    with open(os.path.join(package_dir, 'genes.txt'), 'rb') as fp:   # Unpickling
+        genes_all = pickle.load(fp)
+        
+    #Create pseudo adata
+    var_pseudo = pd.DataFrame({'Gene_Names': genes_all})
+    var_pseudo = var_pseudo.set_index(['Gene_Names'])
+    obs_pseudo = pd.DataFrame({'Cols':[1]})
+    obs_pseudo = obs_pseudo.set_index('Cols')
+    matrix_pseudo = np.zeros((1,28517))
+    matrix_pseudo = sparse.csr_matrix(matrix_pseudo)
+    adata_training = sc.AnnData(matrix_pseudo, obs_pseudo, var_pseudo)
     
     #Slice genes in the test dataset and add genes from training dataset if they are unique to the training dataset. 
-    adata_test_copy = adata_test.copy()
-    genes_slice = [i for i in adata_test_copy.var.index if i in genes_training]
+    genes_training = [i for i in adata.var.index if i in genes_all]
+    adata_test = adata.copy()
     #Rearrange the test dataset to match the index of training.
-    adata_test_copy = adata_test_copy[:,genes_slice]
+    adata_test = adata_test[:,genes_training].copy()
+    sex_classes = {'F':0, 'M':1}
     
-    print('Preparing Test Adata')
-    
-    #Create Pseudo adata
-    var_pseudo = pd.DataFrame({'Gene_Names': genes_training}).set_index(['Gene_Names'])
-    obs_pseudo = pd.DataFrame({'Cols':[1]}).set_index('Cols')
-    matrix_pseudo = np.zeros((1,(len(genes_training))))
-    adata_pseudo_train = sc.AnnData(sparse.csr_matrix(matrix_pseudo), obs_pseudo, var_pseudo)
+    print('Imported Essentials')
     
     #Merge the datasets 
-    adata_test_copy.obs['Set'] = 'Test'
-    adata_pseudo_train.obs['Set'] = 'Training'
-    adata_list = [adata_pseudo_train, adata_test_copy]
+    adata_test.obs['Set'] = 'Test'
+    adata_training.obs['Set'] = 'Training'
+    adata_list = [adata_training, adata_test]
     adata_merged = ad.concat(adata_list, join='outer')
     #Split the dataset
-    adata_test_copy = adata_merged[adata_merged.obs.Set == 'Test']
+    adata_test = adata_merged[adata_merged.obs.Set == 'Test']
     
     #Reindex test dataset
-    adata_test_copy.var = adata_test_copy.var.set_index(adata_pseudo_train.var.index)
+    adata_test.var = adata_test.var.set_index(adata_training.var.index)
     
-    print('Test Anndata Modified For The Model')
+    print('Anndata Modified For The Model')
         
     #Make the test matrix
-    sex_classes = {'F':0, 'M':1}
-    if ('Sex' or 'sex' or 'SEX') in adata_test.obs_keys():
-        adata_test_copy.obs['Sex_Class'] = adata_test.obs.Sex.map(sex_classes).astype('category')
-        test = xgb.DMatrix(adata_test_copy.X, label=adata_test_copy.obs.Sex_Class)
+    if ('Sex' or 'sex') in adata.obs_keys():
+        adata_test.obs['Sex_Class'] = adata.obs.Sex.map(sex_classes).astype('category')
+        test = xgb.DMatrix(adata_test.X, label=adata_test.obs.Sex_Class)
     else:
-        test = xgb.DMatrix(adata_test_copy.X)
-    
+        test = xgb.DMatrix(adata_test.X)
     #Predictions
     predictions_softmax = model_softmax.predict(test)
     predictions_softprob = model_softprob.predict(test)
@@ -173,47 +78,48 @@ def sex_classifier_universal(adata_test, model_softmax, model_softprob, class_pr
     print('Sex Prediction Complete')
     
     #Adding columns to the adata
-    adata_test.obs['Predictions'] =  predictions_softmax
+    adata.obs['Predictions'] =  predictions_softmax
     sex_classes_2 = {0: 'F', 1: 'M'}
-    adata_test.obs['Predictions'] = adata_test.obs.Predictions.map(sex_classes_2).astype('category')
-    adata_test.obs['Predictions_Probability_Female'] = predictions_softprob.T[0]
-    adata_test.obs['Predictions_Probability_Male'] = predictions_softprob.T[1]
-    adata_test_copy.obs['Predictions'] = predictions_softmax
-    adata_test_copy.obs['Class_Prediction'] = np.where((adata_test_copy.obs['Predictions'] == 0.0), 
-                                                  adata_test.obs.Predictions_Probability_Female, 
-                                                  adata_test.obs.Predictions_Probability_Male)
-    print('Adding Columns to the Test Anndata')
+    adata.obs['Predictions'] = adata.obs.Predictions.map(sex_classes_2).astype('category')
+    adata.obs['Predictions_Probability_Female'] = predictions_softprob.T[0]
+    adata.obs['Predictions_Probability_Male'] = predictions_softprob.T[1]
+    adata_test.obs['Predictions'] = predictions_softmax
+    adata_test.obs['Class_Prediction'] = np.where((adata_test.obs['Predictions'] == 0.0), 
+                                                  adata.obs.Predictions_Probability_Female, 
+                                                  adata.obs.Predictions_Probability_Male)
+    print('Adding Columns to the Anndata')
     
     #If loop for adding the class prediction of the provided sex data 
-    if ('Sex' or 'sex' or 'SEX') in adata_test.obs_keys():
-        conditions = [((adata_test_copy.obs.Sex_Class == adata_test_copy.obs.Predictions) & (adata_test_copy.obs.Class_Prediction > class_prob_cutoff)),
-          ((adata_test_copy.obs.Sex_Class != adata_test_copy.obs.Predictions) & (adata_test_copy.obs.Class_Prediction > class_prob_cutoff)), 
-          ((adata_test_copy.obs.Sex_Class == adata_test_copy.obs.Predictions) & (adata_test_copy.obs.Class_Prediction < class_prob_cutoff)),
-          ((adata_test_copy.obs.Sex_Class != adata_test_copy.obs.Predictions) & (adata_test_copy.obs.Class_Prediction < class_prob_cutoff))]
+    if ('Sex' or 'sex') in adata.obs_keys():
+        conditions = [((adata_test.obs.Sex_Class == adata_test.obs.Predictions) & (adata_test.obs.Class_Prediction > class_prob_cutoff)),
+          ((adata_test.obs.Sex_Class != adata_test.obs.Predictions) & (adata_test.obs.Class_Prediction > class_prob_cutoff)), 
+          ((adata_test.obs.Sex_Class == adata_test.obs.Predictions) & (adata_test.obs.Class_Prediction < class_prob_cutoff)),
+          ((adata_test.obs.Sex_Class != adata_test.obs.Predictions) & (adata_test.obs.Class_Prediction < class_prob_cutoff))]
         choices = [ "True Positive", 'False Positive', 'False Negative' , 'True Negative']
-        adata_test.obs['Condition'] = np.select(conditions, choices, default=np.nan)
+        adata.obs['Condition'] = np.select(conditions, choices, default=np.nan)
         #Accuracy Score
-        adata_test_copy_males = adata_test_copy[adata_test_copy.obs.Sex == 'M']
-        acc_score_males = accuracy_score(adata_test_copy_males.obs.Sex_Class, adata_test_copy_males.obs.Predictions)
-        print('The accuracy_score for males for universally trained model is {}'.format(acc_score_males))
-        adata_test_copy_females = adata_test_copy[adata_test_copy.obs.Sex == 'F']
-        acc_score_females = accuracy_score(adata_test_copy_females.obs.Sex_Class, adata_test_copy_females.obs.Predictions)
-        print('The accuracy_score for females for universally trained model is {}'.format(acc_score_females))
+        adata_test_males = adata_test[adata_test.obs.Sex == 'M']
+        acc_score_males = accuracy_score(adata_test_males.obs.Sex_Class, adata_test_males.obs.Predictions)
+        print('The accuracy_score for males for trained model is {}'.format(acc_score_males))
+        adata_test_females = adata_test[adata_test.obs.Sex == 'F']
+        acc_score_females = accuracy_score(adata_test_females.obs.Sex_Class, adata_test_females.obs.Predictions)
+        print('The accuracy_score for females for trained model is {}'.format(acc_score_females))
     else:
         pass
     
+    print('Prediction Completed')
+    
     #Add ROC Curve and ROC-AUC Value
-    auroc = roc_auc_score(adata_test_copy.obs.Sex_Class, adata_test_copy.obs.Predictions)
-    fpr, tpr, thresholds = roc_curve(adata_test_copy.obs.Sex_Class, adata_test_copy.obs.Predictions)
+    auroc = roc_auc_score(adata_test.obs.Sex_Class, adata_test.obs.Predictions)
+    fpr, tpr, thresholds = roc_curve(adata_test.obs.Sex_Class, adata_test.obs.Predictions)
     #Plot the ROC Curve
     plt.figure(figsize=(5,5))
     plt.plot(fpr, tpr, linestyle = '-', label = 'ROC_AUC_Curve %0.3f' % auroc)
     plt.legend(loc='lower right')
     plt.show()
-    print('Prediction Completed')
-    print("--- %s mins ---" % int((time.time() - start_time)/60))
     
-    
+    print("--- %s secs ---" % int((time.time() - start_time)))
+
 def misclassified(adata, min_ncounts=1100, min_genes=300, min_mtfrac=0.04):
     """Returns dataframe for misclassified cells"""
     print('Initializing')
@@ -362,7 +268,6 @@ def ambiguously_classified(adata,min_ncounts=1100, min_genes=300, min_mtfrac=0.0
         return None
     else:
         pass
-    
     if 'Annotation' not in adata_test.obs.keys():
             #Annotate the cells
             azimuth_markers = pd.read_csv((os.path.join(package_dir, 'azimuth_markers_MACA.csv')), index_col=['Unnamed: 0'])
@@ -483,8 +388,7 @@ def ambiguously_classified(adata,min_ncounts=1100, min_genes=300, min_mtfrac=0.0
     chosen_cell_type = mis_pred_df_temp.iloc[(np.argmax(mis_pred_df_temp['Perc_Unexplained_Total_Pop'])), :].Annotation
     print('Low quality cells detected and dataframe created')
     print("--- %s mins ---" % int((time.time() - start_time)/60))
-    return mis_pred_df 
-    
+    return mis_pred_df   
 
 def plot_avg_gene_expression(adata):
     """Returns average gene expression of differentially expressed genes for correctly and incorrectly classified cells"""
@@ -536,7 +440,7 @@ def plot_avg_gene_expression(adata):
         print('Dataframes created')
         print('Plotting')
         #Plots
-        fig, ((ax1,ax2),(ax3,ax4)) = plt.subplots(nrows=2, ncols=2,  figsize=(20,10))
+        fig, ((ax1,ax2),(ax3,ax4)) = plt.subplots(nrows=2, ncols=2, figsize=(20,10))
         fig.subplots_adjust(hspace=0.5, wspace=0.001)
         sb.heatmap(df_females_correct, ax=ax1, cmap='GnBu', alpha=0.6, cbar=False).set(title="Correct Females" )
         fig.colorbar(ax1.collections[0], ax=ax1,location="left", use_gridspec=False, pad=0.5, shrink=0.5)
@@ -558,4 +462,4 @@ def plot_avg_gene_expression(adata):
         return None
     
     
-        
+
